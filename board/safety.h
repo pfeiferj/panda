b27c01dffbf61eb87461aa39ebe7a86a00ea2288
Jacob Pfeifer <jacob@pfeifer.dev>
2024-05-05 21:25:46 -0400

    never mind

--- board/safety.h
+++ board/safety.h
@@ -666,8 +666,8 @@ bool steer_angle_cmd_checks(int desired_angle, bool steer_control_enabled, const
     int delta_angle_down = (interpolate(limits.angle_rate_down_lookup, (vehicle_speed.min / VEHICLE_SPEED_FACTOR) - 1.) * limits.angle_deg_to_can) + 1.;
 
     // allow down limits at zero since small floats will be rounded to 0
-    int highest_desired_angle = angle_meas.values[MAX_SAMPLE_VALS-1] + ((angle_meas.values[MAX_SAMPLE_VALS-1] > 0) ? delta_angle_up : delta_angle_down);
-    int lowest_desired_angle = angle_meas.values[MAX_SAMPLE_VALS-1] - ((angle_meas.values[MAX_SAMPLE_VALS-1] >= 0) ? delta_angle_down : delta_angle_up);
+    int highest_desired_angle = desired_angle_last + ((desired_angle_last > 0) ? delta_angle_up : delta_angle_down);
+    int lowest_desired_angle = desired_angle_last - ((desired_angle_last >= 0) ? delta_angle_down : delta_angle_up);
 
     // check that commanded angle value isn't too far from measured, used to limit torque for some safety modes
     // ensure we start moving in direction of meas while respecting rate limits if error is exceeded
